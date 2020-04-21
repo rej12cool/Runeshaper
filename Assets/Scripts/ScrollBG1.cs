5849594bbf7731d4b4952f6857fe13dbf36a2e90
Kateri L <kateri.lalicker@tufts.edu>
2020-04-20 20:34:41 -0400

    Parallax Scrolling

---
+++ Assets/Scripts/ScrollBG1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBG1 : MonoBehaviour
+{
+    [SerializeField]
+    private Transform centerBackground;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= centerBackground.position.x + 21.21f)
+            centerBackground.position = new Vector2(centerBackground.position.x + 21.21f, transform.position.y);
+
+        else if (transform.position.x <= centerBackground.position.x - 21.21)
+            centerBackground.position = new Vector2(centerBackground.position.x - 21.21f, transform.position.y);
+    }
+}
---
+++ Assets/Scripts/ScrollFG.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollFG : MonoBehaviour
+{
+    [SerializeField]
+    private Transform centerBackground;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= centerBackground.position.x + 28.43f)
+            centerBackground.position = new Vector2(centerBackground.position.x + 28.43f, transform.position.y);
+
+        else if (transform.position.x <= centerBackground.position.x - 28.43f)
+            centerBackground.position = new Vector2(centerBackground.position.x - 28.43f, transform.position.y);
+    }
+}
