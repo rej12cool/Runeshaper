e0ffeaa02b32e18320bb82c036b639a31e237fcc
bbell03 <46859217+bbell03@users.noreply.github.com>
2020-04-28 14:07:00 -0400

    Menu
    basic menu/little glitchy

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+
+    public void RestartGame() {
+        SceneManager.LoadScene("Jump Tutorial");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
