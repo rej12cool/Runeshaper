1603d3e69080f927523820b44cc30ad8144a0b48
Tresha Glenister <treshaglenister@tresha-glenisters-macbook-2.local>
2020-05-03 11:47:38 -0400

    Better rune placement mode, new rune types, and a few new levels

---
+++ Assets/Scripts/Runes/RuneCount.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RuneCount : MonoBehaviour
+{
+	// The rune placement script
+	public RunePlacement rp;
+
+	// The buttons for each rune (null if none)
+	public Button airBtn;
+	public Button waterBtn;
+	public Button fireBtn;
+
+	// The text element for the count of each rune available to place
+	public Text airCountTxt;
+	public Text waterCountTxt;
+	public Text fireCountTxt;
+
+	// The number of each type that can be placed
+	public int airNum;
+	public int waterNum;
+	public int fireNum;
+
+	// The tile highlight sprite for each rune
+	public Sprite airS;
+	public Sprite waterS;
+	public Sprite fireS;
+	public Sprite noneS;
+
+	// The currently selected type of rune
+	private Rune.RuneType currSelected = Rune.RuneType.AIR;
+	// Boolean that tells if out of runes or not
+	private bool noneToPlace = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set up all the buttons
+        UpdateButton(airBtn, airCountTxt, airNum);
+        UpdateButton(waterBtn, waterCountTxt, waterNum);
+        UpdateButton(fireBtn, fireCountTxt, fireNum);
+
+        // Set up the listeners
+        airBtn.onClick.AddListener (() => {SelectAir ();});
+        waterBtn.onClick.AddListener (() => {SelectWater ();});
+        fireBtn.onClick.AddListener (() => {SelectFire ();});
+
+        // Set up default selected rune in RunePlacement
+        if (airNum > 0)
+        {
+        	SelectAir();
+        }
+        else if (waterNum > 0)
+        {
+        	SelectWater();
+        }
+        else if (fireNum > 0)
+        {
+        	SelectFire();
+        }
+        else
+        {
+        	rp.UpdateRuneAvailability(true);
+        	rp.UpdateRuneType("", "", noneS);
+        	noneToPlace = true;
+        }
+    }
+
+    // Called by RunePlacement when placing a rune (subtract from count)
+    public void PlacedARune()
+    {
+    	bool downToZero = false;
+
+    	if (currSelected == Rune.RuneType.AIR)
+    	{
+    		airNum--;
+    		UpdateButton(airBtn, airCountTxt, airNum);
+    		// If out of runes, select a different type or NONE
+    		if (airNum <= 0)
+    			downToZero = true;
+    	}
+    	if (currSelected == Rune.RuneType.WATER)
+    	{
+    		waterNum--;
+    		UpdateButton(waterBtn, waterCountTxt, waterNum);
+    		// If out of runes, select a different type or NONE
+    		if (waterNum <= 0)
+    			downToZero = true;
+    	}
+    	if (currSelected == Rune.RuneType.FIRE)
+    	{
+    		fireNum--;
+    		UpdateButton(fireBtn, fireCountTxt, fireNum);
+    		// If out of runes, select a different type or NONE
+    		if (fireNum <= 0)
+    			downToZero = true;
+    	}
+
+    	// Either select one of the remaining types or disable placing
+    	if (downToZero)
+    	{
+			if (airNum > 0)
+			{
+				SelectAir();
+			}
+			else if (waterNum > 0)
+			{
+				SelectWater();
+			}
+			else if (fireNum > 0)
+			{
+				SelectFire();
+			}
+			else
+			{
+				rp.UpdateRuneAvailability(true);
+				rp.UpdateRuneType("", "", noneS);
+				noneToPlace = true;
+			}
+    	}
+    }
+
+    // Called by RunePlacement with deleting a rune (add 1 to count)
+    public void RemovedARune(Rune.RuneType type)
+    {
+    	if (type == Rune.RuneType.AIR)
+    	{
+    		airNum++;
+    		UpdateButton(airBtn, airCountTxt, airNum);
+    		// If was out of runes, select this type
+    		if (noneToPlace)
+    		{
+    			noneToPlace = false;
+    			rp.UpdateRuneAvailability(false);
+    			SelectAir();
+    		}
+    	}
+    	if (type == Rune.RuneType.WATER)
+    	{
+    		waterNum++;
+    		UpdateButton(waterBtn, waterCountTxt, waterNum);
+    		// If was out of runes, select this type
+    		if (noneToPlace)
+    		{
+    			noneToPlace = false;
+    			rp.UpdateRuneAvailability(false);
+    			SelectWater();
+    		}
+    	}
+    	if (type == Rune.RuneType.FIRE)
+    	{
+    		fireNum++;
+    		UpdateButton(fireBtn, fireCountTxt, fireNum);
+    		// If was out of runes, select this type
+    		if (noneToPlace)
+    		{
+    			noneToPlace = false;
+    			rp.UpdateRuneAvailability(false);
+    			SelectFire();
+    		}
+    	}
+    }
+
+    // Called when the air button is clicked
+    public void SelectAir()
+    {
+    	rp.UpdateRuneType("Prefabs/Runes/AirRune", "Prefabs/PlacedRuneTiles/AirRuneTile", airS);
+    	currSelected = Rune.RuneType.AIR;
+    }
+
+    // Called when the water button is clicked
+    public void SelectWater()
+    {
+    	rp.UpdateRuneType("Prefabs/Runes/WaterRune", "Prefabs/PlacedRuneTiles/WaterRuneTile", waterS);
+    	currSelected = Rune.RuneType.WATER;
+    }
+
+    // Called when the fire button is clicked
+    public void SelectFire()
+    {
+    	rp.UpdateRuneType("Prefabs/Runes/FireRune", "Prefabs/PlacedRuneTiles/FireRuneTile", fireS);
+    	currSelected = Rune.RuneType.FIRE;
+    }
+
+
+    // Updates a button and its text based on the number of runes to place given
+    void UpdateButton(Button btn, Text txt, int num)
+    {
+    	// Stop if the button doesn't exist
+    	if (btn == null)
+    	{
+    		return;
+    	}
+
+    	// If no runes left, disable the button
+    	if (num <= 0)
+    	{
+    		btn.interactable = false;
+    	}
+    	// Otherwise, allow to be clicked
+    	else
+    	{
+    		btn.interactable = true;
+    	}
+
+    	// Display the number remaining in the 'Count' text
+    	txt.text = num.ToString();
+    }
+}
--- Assets/Scripts/Runes/RunePlacement.cs
+++ Assets/Scripts/Runes/RunePlacement.cs
@@ -8,6 +8,13 @@ public class RunePlacement : MonoBehaviour
 {
 	public GameObject selectHighlight; // The sprite with that adds a box around a selected tile
     public Slider rotationSlider; // The slider that is shown when a rune is selected
+    public Button deleteButton; // The button to delete a rune tile
+    public RuneCount rc; // The RuneCount script
+
+    // Variables changed by RuneCount.cs
+    private string runePrefabPath = "";     // the file path of the prefab of currently selected rune type
+    private string iconPrefabPath = "";     // the file path of the tile icon for the current type
+    private bool noneToPlace = false;   // whether or not the user can place any more runes
 
 	private TilemapCollider2D env;	// The environment (aka Tilemap collision area) that can place runes in
     private BoxCollider2D sliderCollider; // The rectangle of the slider, used to ignore clicking a tile while adjusting the slider
@@ -16,7 +23,7 @@ public class RunePlacement : MonoBehaviour
 	private bool hovering = false;	// Determines if cursor is hovering over a placeable tile
 	private Vector2 tilePos = new Vector2(0f ,0f);	// The position of the active tile
 	private bool selected = false;	// Determines if a tile is currently selected
-    private PlacedRune selRune = new PlacedRune(null, new Vector2(0f, 0f), 0f, 0f, 0f); // The info for the currently selected rune
+    private PlacedRune selRune = new PlacedRune(null, new Vector2(0f, 0f), 0f, 0f, 0f, null); // The info for the currently selected rune
 	private Vector2 lastClickTile = new Vector2(0f, 0f); // The last tile that was clicked, used to avoid repeating the PlaceRune func
     private bool resetingSlider = false; // True if SliderChangeCheck should ignore changes to slider at the current moment
 
@@ -30,14 +37,16 @@ public class RunePlacement : MonoBehaviour
 		public float curr_rot;
 		public float min_rot;
 		public float max_rot;
+        public GameObject icon;
 
-		public PlacedRune(Rune r, Vector2 t, float rot, float mn, float mx)
+		public PlacedRune(Rune r, Vector2 t, float rot, float mn, float mx, GameObject i)
 		{
 			this.rune = r;
 			this.tile = t;
 			this.curr_rot = rot;
 			this.min_rot = mn;
 			this.max_rot = mx;
+            this.icon = i;
 		}
 	}
 
@@ -50,6 +59,9 @@ public class RunePlacement : MonoBehaviour
 
         //Adds a listener to the rotation slider and calls the rotate function when changed
         rotationSlider.onValueChanged.AddListener (delegate {SliderChangeCheck ();});
+
+        //Adds a listener to the delete button for when it is pressed
+        deleteButton.onClick.AddListener (() => {DeleteRune ();});
     }
 
     // Update is called once per frame
@@ -100,6 +112,11 @@ public class RunePlacement : MonoBehaviour
         // If not, place one first
         if (!runeExists)
         {
+            // If out of runes to place, stop instead
+            if (noneToPlace)
+            {
+                return;
+            }
             selRune = PlaceRune(tile);
         }
 
@@ -126,11 +143,41 @@ public class RunePlacement : MonoBehaviour
 
         // Tell SliderChangeCheck() to start listening for changes again
         resetingSlider = false;
+
+        // Move the delete button on top of the tile and show it
+        deleteButton.gameObject.transform.position = tile;
+        deleteButton.gameObject.SetActive(true);
+    }
+
+    // Invoked when the delete button is clicked
+    public void DeleteRune()
+    {
+        // Delete the tile icon
+        Destroy(selRune.icon);
+
+        // Remove the currently selected rune from the list
+        placedRuneList.Remove(selRune);
+
+        // Let RuneCOunt know we deleted one
+        rc.RemovedARune(selRune.rune.type);
+
+        // Call the delete function for the rune
+        selRune.rune.DestroyRune();
+
+        // Hide the slider and delete button
+        deleteButton.gameObject.SetActive(false);
+        rotationSlider.gameObject.SetActive(false);
+
+        // Unselect this tile
+        selRune = new PlacedRune(null, new Vector2(0f, 0f), 0f, 0f, 0f, null);
+        selected = false;
+        lastClickTile = new Vector2(0f, 0f);
     }
 
     // Invoked when the value of the slider changes.
     public void SliderChangeCheck()
     {
+        // Do nothing if the code is changing the slider right now
         if (resetingSlider)
         {
             return;
@@ -145,8 +192,10 @@ public class RunePlacement : MonoBehaviour
         // Rotate this rune by that change
         selRune.rune.QueueRotation(deltaRot);
 
-        // Update the selected rune's struct
+        // Update the selected rune's struct and the copy in the list
+        placedRuneList.Remove(selRune);
         selRune.curr_rot = rotationSlider.value;
+        placedRuneList.Add(selRune);
     }
 
     // Begins hovering on the tile at pos if it is placeable 
@@ -192,9 +241,14 @@ public class RunePlacement : MonoBehaviour
     	hovering = updateHover;
     }
 
-    // Returns true if tile is placeable (has at least one empty adjacent tile) and false otherwise
+    // Returns true if tile is placeable (has at least one empty adjacent tile) and isn't already selected;
+    // Returns false otherwise
     bool IsPlaceable(Vector2 tile)
     {
+        // Check if this tile is already selected
+        if (tile == selRune.tile)
+            return false;
+
     	Vector2 topTile = new Vector2(tile.x, tile.y + 1f);
     	Vector2 bottomTile = new Vector2(tile.x, tile.y - 1f);
     	Vector2 leftTile = new Vector2(tile.x - 1f, tile.y);
@@ -243,8 +297,8 @@ public class RunePlacement : MonoBehaviour
     	}
 
 
-    	// Pick the type of rune and get its prefab
-    	prefab = (GameObject)Resources.Load("Prefabs/Runes/AirRune", typeof(GameObject));
+    	// Get the prefab for the current rune type
+    	prefab = (GameObject)Resources.Load(runePrefabPath, typeof(GameObject));
 
 	    // Instantiate a new (inactive) rune from prefab
 	    GameObject newRune = Instantiate(prefab, runeBase, Quaternion.Euler(0f, 0f, rot), GameObject.FindWithTag("AllRunes").transform);
@@ -268,9 +322,16 @@ public class RunePlacement : MonoBehaviour
 	    	max += 1;
 	    }
 
-	    // Put the rune, its position, the min-max rotations, and curr rot into the array
-        PlacedRune rinfo = new PlacedRune(newRune.GetComponent<Rune>(), tile, rot, min, max);
+        // Make a new tile icon for this rune
+        prefab = (GameObject)Resources.Load(iconPrefabPath, typeof(GameObject));
+        GameObject newTile = Instantiate(prefab, tile, Quaternion.Euler(0f, 0f, 0f), transform.parent);
+
+	    // Put the rune, its position, the min-max rotations, curr rot, and tile into the array
+        PlacedRune rinfo = new PlacedRune(newRune.GetComponent<Rune>(), tile, rot, min, max, newTile);
 	    placedRuneList.Add(rinfo);
+
+        // Let RuneCount know we consumed one rune
+        rc.PlacedARune();
 
 	    return rinfo;
     }
@@ -279,6 +340,24 @@ public class RunePlacement : MonoBehaviour
     private Vector2 RotToVector(float rot)
     {
     	return new Vector2((-1 * Mathf.Sin(Mathf.PI * rot / 180)), Mathf.Cos(Mathf.PI * rot / 180));
+    }
+
+    // Called by RuneCount.cs, changes the rune type being placed and the icon for the title highlight
+    public void UpdateRuneType(string filepathR, string filepathI, Sprite newS)
+    {
+        // Update the prefab paths
+        runePrefabPath = filepathR;
+        iconPrefabPath = filepathI;
+
+        // Change the cursor tile highlight sprite
+        selectHighlight.GetComponent<SpriteRenderer>().sprite = newS;
+    }
+
+    // Called by RuneCount.cs, notifies this script if the player is out of runes or not
+    public void UpdateRuneAvailability(bool outOfRunes)
+    {
+        // Sets to true if RuneCount is out of runes
+        noneToPlace = outOfRunes;
     }
 
     // Turns rune placement mode on or off (true or false)
@@ -296,15 +375,34 @@ public class RunePlacement : MonoBehaviour
             selected = false;
             hovering = false;
             lastClickTile = new Vector2(0f, 0f);
+
+            // Show all icons for each rune
+            foreach (PlacedRune p in placedRuneList)
+            {
+                p.icon.SetActive(true);
+            }
+
+            // Show the rune selection UI
+            rc.gameObject.SetActive(true);
 		}
 		else
 		{
 			// Hide the cursor
 			Cursor.visible = false;
 
-			// Turn off the highlight and slider
+			// Turn off the highlight, slider, and button
 			selectHighlight.GetComponent<SpriteRenderer>().enabled = false;
             rotationSlider.gameObject.SetActive(false);
+            deleteButton.gameObject.SetActive(false);
+
+            // Hide all icons for each rune
+            foreach (PlacedRune p in placedRuneList)
+            {
+                p.icon.SetActive(false);
+            }
+
+            // Hide the rune selection UI
+            rc.gameObject.SetActive(false);
 		}
     }
 }
