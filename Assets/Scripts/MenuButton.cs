1900c9739bec919ce8ee0bcfe175a75adbdbd75b
bbell03 <46859217+bbell03@users.noreply.github.com>
2020-05-07 19:38:48 -0400

    Updated Main Menu with Level Selector

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -5,8 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour {
 
-    public void RestartGame() {
+    public void StartGame() {
         SceneManager.LoadScene("1-0");
+    }
+
+    public void Levels() {
+        SceneManager.LoadScene("Levels");
+    }
+
+    public void LevelOne(int x)
+    {
+        LevelSelect(1, x);
+    }
+
+    public void LevelTwo(int x)
+    {
+        LevelSelect(2, x);
+    }
+
+    public void LevelThree(int x)
+    {
+        LevelSelect(3, x);
+    }
+
+    public void LevelFour(int x)
+    {
+        LevelSelect(4, x);
+    }
+
+    public void LevelFive(int x)
+    {
+        LevelSelect(5, x);
+    }
+    public void LevelSelect(int level, int x) {
+
+        SceneManager.LoadScene(level + "-" + x);
     }
 
     public void QuitGame() {
